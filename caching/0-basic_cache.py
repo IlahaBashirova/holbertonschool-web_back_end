877f2ec69b8daa27e04567896e668dc1c8d7c0a4
IlahaBashirova <11861@holbertonstudents.com>
2025-11-08 09:49:23 +0400

    wfd

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ BasicCache module.
 """
-from base_caching import  BaseCaching
+from base_caching import BaseCaching
+
 
 class BasicCache(BaseCaching):
     """ BasicCache defines a basic caching system
@@ -20,3 +21,4 @@ class BasicCache(BaseCaching):
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data.get(key)
+    
