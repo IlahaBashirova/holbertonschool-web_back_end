17cf6797de1fb5e04784894ecdbe2d55f9f50d4a
IlahaBashirova <11861@holbertonstudents.com>
2025-11-08 09:07:28 +0400

    furst task

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" BasicCache module
+"""
+from anyio import key
+from .base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """ BasicCache defines a basic caching system
+    """
+
+    def put(self,key,item):
+        """Add an item to the dictionary
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self,key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
