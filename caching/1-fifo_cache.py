39fb5433a4a3d96a79b7800a333c49d7bbfe0a35
IlahaBashirova <11861@holbertonstudents.com>
2025-11-08 12:01:21 +0400

    eygfjhw'
    git add .

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ FIFO caching.
 """
+from anyio import key
 from base_caching import BaseCaching
 
 
@@ -9,22 +10,24 @@ def __init__(self):
     """
     super().__init__()
 
-def put(self, key, item):
-    """Add an item in the cache.
-    """
-    if key is None or item is None:
-        return 
-    if len(self.cache_data)>=BaseCaching.MAX_ITEMS:
-        discarder_item=self.cache_data.popitem(last=False)
-        print("DISCARD: ", f"{discarder_item[0]}")
-    self.cache_data[key]=item
-    
-def get(self, key):
-    """Get an item by key.
-    """
-    if key is None or key not in self.cache_data:
-        return None
-    return self.cache_data.get(key)
+class FIFOCache(BaseCaching):
+    """ FIFOCache defines a caching system
+        that follows the FIFO algorithm
+    """ 
+    def put(self, key, item):
+        """Add an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discarder_key, discarder_item = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {discarder_key}")
+        self.cache_data[key] = item
 
-
-    
+    def get(self, key):
+        """Get an item by key.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
+   
