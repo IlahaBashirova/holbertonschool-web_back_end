d80999deb71fc5e494e04b62190b9de504ebda10
IlahaBashirova <11861@holbertonstudents.com>
2025-11-29 22:28:20 +0400

    uyrfjh

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -19,13 +19,8 @@ def filter_datum(fields: List[str],
                  message: str,
                  separator: str) -> str:
     """Return message with values of specified fields redacted."""
-    sep = re.escape(separator)
-    pattern = f"({'|'.join(map(re.escape, fields))})=[^{sep}]*"
-    return re.sub(
-        pattern,
-        lambda m: m.group(1) + '=' + redaction,
-        message,
-    )
+    pattern = "({})=[^{}]*".format("|".join(fields), separator)
+    return re.sub(pattern, r"\1={}".format(redaction), message)
 
 
 class RedactingFormatter(logging.Formatter):
@@ -37,16 +32,14 @@ class RedactingFormatter(logging.Formatter):
 
     def __init__(self, fields: List[str]) -> None:
         """Initialize the formatter with a list of fields to redact."""
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+        super().__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """Filter values in incoming log records."""
-        formatted = super(RedactingFormatter, self).format(record)
-        return filter_datum(self.fields,
-                            self.REDACTION,
-                            formatted,
-                            self.SEPARATOR)
+        """Filter values in the log record."""
+        msg = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            msg, self.SEPARATOR)
 
 
 def get_logger() -> logging.Logger:
@@ -56,12 +49,10 @@ def get_logger() -> logging.Logger:
     logger.propagate = False
 
     handler = logging.StreamHandler()
-    formatter = RedactingFormatter(list(PII_FIELDS))
-    handler.setFormatter(formatter)
+    handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
 
     logger.handlers = []
     logger.addHandler(handler)
-
     return logger
 
 
@@ -78,3 +69,29 @@ def get_db() -> MySQLConnection:
         host=host,
         database=db_name,
     )
+
+
+def main() -> None:
+    """Obtain a DB connection and display filtered rows from users table."""
+    db = get_db()
+    cursor = db.cursor()
+    query = ("SELECT name, email, phone, ssn, password, ip, "
+             "last_login, user_agent FROM users;")
+    cursor.execute(query)
+
+    logger = get_logger()
+    fields = ("name", "email", "phone", "ssn",
+              "password", "ip", "last_login", "user_agent")
+
+    for row in cursor:
+        message = "; ".join(
+            "{}={}".format(field, value) for field, value in zip(fields, row)
+        )
+        logger.info(message)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
