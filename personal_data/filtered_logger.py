54d302234f3884a6b4e1b73738f617c8b5b015a3
IlahaBashirova <11861@holbertonstudents.com>
2025-11-29 18:50:22 +0400

    filtered logger done

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module for filtering sensitive fields from log messages."""
 
+import logging
 import re
 from typing import List
 
@@ -12,8 +13,24 @@ def filter_datum(fields: List[str],
     """Return message with values of specified fields redacted."""
     sep = re.escape(separator)
     pattern = f"({'|'.join(map(re.escape, fields))})=[^{sep}]*"
+    return re.sub(pattern, lambda m: m.group(1) + '=' + redaction, message)
 
-    def repl(m):
-        return m.group(1) + "=" + redaction
 
-    return re.sub(pattern, repl, message)
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class."""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialize the formatter with a list of fields to redact."""
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filter values in the log record."""
+        msg = filter_datum(self.fields, self.REDACTION,
+                           record.getMessage(), self.SEPARATOR)
+        record.msg = msg
+        return super().format(record)
