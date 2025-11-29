6a013534d95bd216fd28dc64c41b2133431c7488
IlahaBashirova <11861@holbertonstudents.com>
2025-11-29 22:24:56 +0400

    uyrfjh

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -37,15 +37,16 @@ class RedactingFormatter(logging.Formatter):
 
     def __init__(self, fields: List[str]) -> None:
         """Initialize the formatter with a list of fields to redact."""
-        super().__init__(self.FORMAT)
+        super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """Filter values in the log record."""
-        msg = filter_datum(self.fields, self.REDACTION,
-                           record.getMessage(), self.SEPARATOR)
-        record.msg = msg
-        return super().format(record)
+        """Filter values in incoming log records."""
+        formatted = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields,
+                            self.REDACTION,
+                            formatted,
+                            self.SEPARATOR)
 
 
 def get_logger() -> logging.Logger:
