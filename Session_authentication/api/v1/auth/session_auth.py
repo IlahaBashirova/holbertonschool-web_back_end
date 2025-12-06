720d08f472141931b8c2499d3323bd4472e53e71
IlahaBashirova <11861@holbertonstudents.com>
2025-12-06 19:36:08 +0400

    done

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,12 @@
 """SessionAuth module"""
 
 from typing import TypeVar
+from urllib import response
 from api.v1.auth.auth import Auth
 import uuid
 from models.user import User
+from flask import jsonify, request
+from os import getenv
 
 
 class SessionAuth(Auth):
@@ -43,3 +46,34 @@ class SessionAuth(Auth):
         if user_id is None:
             return None
         return User.get(user_id)
+
+    @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+    def auth_session_login():
+        """
+        POST /api/v1/auth_session/login
+        Login using email + password, create a session,
+        return the user JSON and set the session cookie.
+        """
+        email = request.form.get("email")
+        password = request.form.get("password")
+        if email is None or email == "":
+            return jsonify({"error": "email missing"}), 400
+        if password is None or password == "":
+            return jsonify({"error": "password missing"}), 400
+        users = User.search({"email": email})
+        if not users:
+            return jsonify({"error": "no user found for this email"}), 404
+        user = users[0]
+        if not user.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
+        from api.v1.app import auth
+
+        session_id = auth.create_session(user.id)
+        response = jsonify(user.to_json())
+        session_name = getenv("SESSION_NAME")
+        if session_name is None:
+            session_name = "_my_session_id"
+
+        response.set_cookie(session_name, session_id)
+
+        return response
