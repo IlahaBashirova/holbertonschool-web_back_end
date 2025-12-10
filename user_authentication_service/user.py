a0c52e259f95e71ac7382389f6632c2c3a2b4460
IlahaBashirova <11861@holbertonstudents.com>
2025-12-10 18:46:39 +0400

    done

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 
-Base = declorative_base()
+Base = declarative_base()
 
 
 class User(Base):
