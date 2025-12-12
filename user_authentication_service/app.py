3d2322778c71d949c4eb6a0d6fb024499fa39f48
IlahaBashirova <11861@holbertonstudents.com>
2025-12-12 22:59:57 +0400

    done

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
 """Basic Flask App
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index():
     """Basic GET endpoint"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    """Register a new user."""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
