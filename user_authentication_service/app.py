041a0c3249ba8d12ddd5535e6d38db6be380714d
IlahaBashirova <11861@holbertonstudents.com>
2025-12-12 16:38:24 +0400

    done

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -5,10 +5,12 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index():
     """Basic GET endpoint"""
-    return jsonify({"message":"Bienvenue"})
+    return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
