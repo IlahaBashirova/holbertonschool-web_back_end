2c5331f2253c2ba55014f2597811d0c6570251bb
IlahaBashirova <11861@holbertonstudents.com>
2025-12-13 19:55:19 +0400

    done

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -59,7 +59,7 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
         return session_id
 
-    def get_user_from_session_id(self, session_id: str | None) -> User:
+    def get_user_from_session_id(self, session_id: str) -> User:
         """Return the User corresponding to a given session_id."""
         if session_id is None:
             return None
