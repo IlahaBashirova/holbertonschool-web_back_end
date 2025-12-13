75ea9bcf6a71ff24075ab91f2a4bde2219453dc8
IlahaBashirova <11861@holbertonstudents.com>
2025-12-13 20:23:50 +0400

    done

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -81,3 +81,13 @@ class Auth:
 
         self._db.update_user(user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Generate a reset password token."""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError("User with this email does not exist")
+        token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=token)
+        return token
