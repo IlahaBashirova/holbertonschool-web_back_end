0d74c5a70640c6878ce75778321cc4b56cde9fdd
IlahaBashirova <11861@holbertonstudents.com>
2025-12-20 14:28:16 +0400

    done

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -40,3 +40,25 @@ class TestGithubOrgClient(unittest.TestCase):
                 client._public_repos_url,
                 "https://api.github.com/orgs/google/repos",
             )
+
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json):
+        """It should return a list of repo names and call dependencies once."""
+        test_payload = [
+            {"name": "repo1"},
+            {"name": "repo2"},
+            {"name": "repo3"},
+        ]
+        mock_get_json.return_value = test_payload
+
+        with patch.object(
+            GithubOrgClient,
+            "_public_repos_url",
+            new_callable=PropertyMock,
+            return_value="http://fake-url.com",
+        ) as mock_repos_url:
+            client = GithubOrgClient("google")
+            self.assertEqual(client.public_repos(), ["repo1", "repo2", "repo3"])
+
+            mock_repos_url.assert_called_once()
+            mock_get_json.assert_called_once_with("http://fake-url.com")
