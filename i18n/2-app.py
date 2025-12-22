100bc70d088490d6d94ce26c958e0badae02725d
IlahaBashirova <11861@holbertonstudents.com>
2025-12-22 16:40:34 +0400

    done

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -18,9 +18,11 @@ app.config.from_object(Config)
 
 babel = Babel(app)
 
+
 def get_locale() -> str:
     """Select the best language match"""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
+
 
 babel.init_app(app, locale_selector=get_locale)
 
