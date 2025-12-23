72dd8a74cfc82ab2c4f06865a9e446cfdf0abd52
IlahaBashirova <11861@holbertonstudents.com>
2025-12-23 16:10:34 +0400

    done

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -30,7 +30,7 @@ babel.init_app(app, locale_selector=get_locale)
 @app.route('/')
 def index():
     """Render index page"""
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
