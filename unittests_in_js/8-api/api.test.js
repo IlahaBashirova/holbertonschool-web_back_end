ca3e8cd5271277c78d689eaa66de61ed21b0af56
IlahaBashirova <11861@holbertonstudents.com>
2026-01-03 22:40:45 +0400

    done

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -2,7 +2,7 @@ const request = require('request');
 const { expect } = require('chai');
 
 describe('Index page', function () {
-  it('should return status code 200', function (done) {
+  it('should return the correct status code', function (done) {
     request.get('http://localhost:7865', (err, res, body) => {
       expect(res.statusCode).to.equal(200);
       done();
@@ -12,29 +12,6 @@ describe('Index page', function () {
   it('should return the correct result', function (done) {
     request.get('http://localhost:7865', (err, res, body) => {
       expect(body).to.equal('Welcome to the payment system');
-      done();
-    });
-  });
-});
-
-describe('Cart page', function () {
-  it('should return status code 200 when id is a number', function (done) {
-    request.get('http://localhost:7865/cart/12', (err, res, body) => {
-      expect(res.statusCode).to.equal(200);
-      done();
-    });
-  });
-
-  it('should return the correct result when id is a number', function (done) {
-    request.get('http://localhost:7865/cart/12', (err, res, body) => {
-      expect(body).to.equal('Payment methods for cart 12');
-      done();
-    });
-  });
-
-  it('should return status code 404 when id is NOT a number', function (done) {
-    request.get('http://localhost:7865/cart/hello', (err, res, body) => {
-      expect(res.statusCode).to.equal(404);
       done();
     });
   });
